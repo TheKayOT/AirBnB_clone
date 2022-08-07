cae9c0d6c006965656e1c71fe3031a50a13c5af1
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-08-07 20:38:49 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Defines the State class."""
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """Represent a state.
+class Review(BaseModel):
+    """Represent a review.
 
     Attributes:
-        name (str): The name of the state.
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
     """
 
-    name = ""
+    place_id = ""
+    user_id = ""
+    text = ""
